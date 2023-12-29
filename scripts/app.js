84ab12f39287068bd74e84a94a24c22348764521
KEVINBRETS <123332955+KEVINBRETS@users.noreply.github.com>
2023-12-29 10:03:26 +0100

    toggle form

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,28 @@
 "use strict";
-// commentaires en francais :
+// On selectionne tous les "items-container" et on les stocke dans un tableau
 const itemsContainer = document.querySelectorAll(".items-container");
-console.log(itemsContainer);
-// on parcours les items-container
+// console.log(itemsContainer);
+let actualContainer, actualBtn, actualUL, actualForm, actualTextInput, actualValidation;
+//
 function addContainerListeners(currentContainer) {
     //
     const currentContainerDeletionBtn = currentContainer.querySelector(".delete-container-btn");
+    const currentAddItemBtn = currentContainer.querySelector(".add-item-btn");
     deleteBtnListeners(currentContainerDeletionBtn);
+    addItemBtnListener(currentAddItemBtn);
 }
+//
 itemsContainer.forEach((container) => {
     addContainerListeners(container);
 });
+//
 function deleteBtnListeners(btn) {
     btn.addEventListener("click", handleContainerDeletion);
 }
+function addItemBtnListener(btn) {
+    btn.addEventListener("click", handleAddItem);
+}
+//
 function handleContainerDeletion(e) {
     const btn = e.target;
     const btnsArray = [
@@ -24,4 +33,30 @@ function handleContainerDeletion(e) {
     ];
     containers[btnsArray.indexOf(btn)].remove();
     console.log("removed !");
+}
+function handleAddItem(e) {
+    const btn = e.target;
+    // si un form est ouvert on le ferme automatiquement pour ouvrir l'actuel
+    if (actualContainer)
+        toggleForm(actualBtn, actualForm, false);
+    setContainerItem(btn);
+    toggleForm(actualBtn, actualForm, true);
+}
+function toggleForm(btn, form, action) {
+    if (!action) {
+        form.style.display = "none";
+        btn.style.display = "block";
+    }
+    else if (action) {
+        form.style.display = "block";
+        btn.style.display = "none";
+    }
+}
+function setContainerItem(btn) {
+    actualBtn = btn;
+    actualContainer = btn.parentElement;
+    actualUL = actualContainer.querySelector("ul");
+    actualForm = actualContainer.querySelector("form");
+    actualTextInput = actualContainer.querySelector("input");
+    actualValidation = actualContainer.querySelector("validation-msg");
 }
--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,46 @@
-// commentaires en francais :
+// On selectionne tous les "items-container" et on les stocke dans un tableau
 const itemsContainer = document.querySelectorAll(
   ".items-container"
 ) as NodeListOf<HTMLDivElement>;
-console.log(itemsContainer);
+// console.log(itemsContainer);
 
-// on parcours les items-container
+let actualContainer: HTMLDivElement,
+  actualBtn: HTMLButtonElement,
+  actualUL: HTMLUListElement,
+  actualForm: HTMLFormElement,
+  actualTextInput: HTMLInputElement,
+  actualValidation: HTMLSpanElement;
+
+//
 function addContainerListeners(currentContainer: HTMLDivElement) {
   //
   const currentContainerDeletionBtn = currentContainer.querySelector(
     ".delete-container-btn"
   ) as HTMLButtonElement;
 
+  const currentAddItemBtn = currentContainer.querySelector(
+    ".add-item-btn"
+  ) as HTMLButtonElement;
+
   deleteBtnListeners(currentContainerDeletionBtn);
+  addItemBtnListener(currentAddItemBtn);
 }
 
+//
 itemsContainer.forEach((container: HTMLDivElement) => {
   addContainerListeners(container);
 });
 
+//
 function deleteBtnListeners(btn: HTMLButtonElement) {
   btn.addEventListener("click", handleContainerDeletion);
 }
 
+function addItemBtnListener(btn: HTMLButtonElement) {
+  btn.addEventListener("click", handleAddItem);
+}
+
+//
 function handleContainerDeletion(e: MouseEvent) {
   const btn = e.target as HTMLButtonElement;
   const btnsArray = [
@@ -32,5 +51,38 @@ function handleContainerDeletion(e: MouseEvent) {
   ] as HTMLDivElement[];
   containers[btnsArray.indexOf(btn)].remove();
   console.log("removed !");
-  
+}
+
+function handleAddItem(e: MouseEvent) {
+  const btn = e.target as HTMLButtonElement;
+  // si un form est ouvert on le ferme automatiquement pour ouvrir l'actuel
+  if (actualContainer) toggleForm(actualBtn, actualForm, false);
+
+  setContainerItem(btn);
+  toggleForm(actualBtn, actualForm, true);
+}
+
+function toggleForm(
+  btn: HTMLButtonElement,
+  form: HTMLFormElement,
+  action: boolean
+) {
+  if (!action) {
+    form.style.display = "none";
+    btn.style.display = "block";
+  } else if (action) {
+    form.style.display = "block";
+    btn.style.display = "none";
+  }
+}
+
+function setContainerItem(btn: HTMLButtonElement) {
+  actualBtn = btn;
+  actualContainer = btn.parentElement as HTMLDivElement;
+  actualUL = actualContainer.querySelector("ul") as HTMLUListElement;
+  actualForm = actualContainer.querySelector("form") as HTMLFormElement;
+  actualTextInput = actualContainer.querySelector("input") as HTMLInputElement;
+  actualValidation = actualContainer.querySelector(
+    "validation-msg"
+  ) as HTMLSpanElement;
 }
